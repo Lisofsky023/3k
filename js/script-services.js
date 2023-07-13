fae927117d7b1aea042695f9d243d9a5596d404e
lisofsky23 <lisofsky23@gmail.com>
2023-07-13 23:04:40 +0400

    3k

--- js/script-services.js
+++ js/script-services.js
@@ -1,99 +1,135 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let animatedTitle = document.getElementsByClassName('services__title');
-    let animatedNumber = document.getElementsByClassName('services__number');
-    let animatedString = document.getElementsByClassName('services__string');
-    let animatedSubtitle = document.getElementsByClassName('services__text-subtitle');
-    let animatedIcon = document.getElementsByClassName('services__icon');
-    let animatedFrame = document.getElementsByClassName('services__frame');
-    let animatedText = document.getElementsByClassName('services__text');
-    let animatedList = document.querySelector('.services__list');
-    
-    let scrollPosition = parseInt(localStorage.getItem('scrollPosition')) || 0;
-    
-    function isElementInViewport(element) {
-      var rect = element.getBoundingClientRect();
-      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-      return rect.top <= windowHeight;
-    }
-    
-    function animateElementsOnScroll() {
-      for (var i = 0; i < animatedTitle.length; i++) {
-        var element = animatedTitle[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('show');
-        }
-      }
-    
-      for (var i = 0; i < animatedNumber.length; i++) {
-        var element = animatedNumber[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('show');
-        }
-      }
-    
-      for (var i = 0; i < animatedString.length; i++) {
-        var element = animatedString[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('show');
-        }
-      }
-    
-      for (var i = 0; i < animatedSubtitle.length; i++) {
-        var element = animatedSubtitle[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('show');
-        }
-      }
-    
-      for (var i = 0; i < animatedIcon.length; i++) {
-        var element = animatedIcon[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('show');
-        }
-      }
-    
-      for (var i = 0; i < animatedFrame.length; i++) {
-        var element = animatedFrame[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('slide');
-        }
-      }
-    
-      for (var i = 0; i < animatedText.length; i++) {
-        var element = animatedText[i];
-    
-        if (isElementInViewport(element)) {
-          element.classList.add('slide-again');
-        }
-      }
-    
-      if (isElementInViewport(animatedList)) {
-        animatedList.classList.add('slide-again');
+  let animatedTitle = document.getElementsByClassName('services__title');
+  let animatedNumber = document.getElementsByClassName('services__number');
+  let animatedString = document.getElementsByClassName('services__string');
+  let animatedSubtitle = document.getElementsByClassName('services__text-subtitle');
+  let animatedIcon = document.getElementsByClassName('services__icon');
+  let animatedFrame = document.getElementsByClassName('services__frame');
+  let animatedText = document.getElementsByClassName('services__text');
+  let animatedList = document.querySelector('.services__list');
+  let aboutUsLink = document.querySelector('.about-us')
+
+  aboutUsLink.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+
+  let scrollPosition = parseInt(localStorage.getItem('scrollPosition')) || 0;
+
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight;
+  }
+
+  function animateElementsOnScroll() {
+    for (var i = 0; i < animatedTitle.length; i++) {
+      var element = animatedTitle[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('show');
       }
     }
-    
-    function saveScrollPosition() {
-      scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-      localStorage.setItem('scrollPosition', scrollPosition);
+
+    for (var i = 0; i < animatedNumber.length; i++) {
+      var element = animatedNumber[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('show');
+      }
     }
-    
-    function restoreScrollPosition() {
-      window.scrollTo(0, scrollPosition);
+
+    for (var i = 0; i < animatedString.length; i++) {
+      var element = animatedString[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('show');
+      }
     }
-    
+
+    for (var i = 0; i < animatedSubtitle.length; i++) {
+      var element = animatedSubtitle[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('show');
+      }
+    }
+
+    for (var i = 0; i < animatedIcon.length; i++) {
+      var element = animatedIcon[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('show');
+      }
+    }
+
+    for (var i = 0; i < animatedFrame.length; i++) {
+      var element = animatedFrame[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('slide');
+      }
+    }
+
+    for (var i = 0; i < animatedText.length; i++) {
+      var element = animatedText[i];
+
+      if (isElementInViewport(element)) {
+        element.classList.add('slide-again');
+      }
+    }
+
+    if (isElementInViewport(animatedList)) {
+      animatedList.classList.add('slide-again');
+    }
+  }
+
+  function saveScrollPosition() {
+    scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    localStorage.setItem('scrollPosition', scrollPosition);
+  }
+
+  function restoreScrollPosition() {
+    window.scrollTo(0, scrollPosition);
+  }
+
+  animateElementsOnScroll();
+  restoreScrollPosition();
+
+  window.addEventListener('scroll', () => {
     animateElementsOnScroll();
-    restoreScrollPosition();
-    
-    window.addEventListener('scroll', () => {
-      animateElementsOnScroll();
-      saveScrollPosition();
+    saveScrollPosition();
+  });
+
+  // back-arrow-button
+  // Получаем ссылки на блоки
+  const backBlock = document.querySelector('.back-block');
+  const firstBlock = document.querySelector('.first');
+  const backBtn = document.querySelector('.back__btn')
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
+    const viewportHeight = window.innerHeight;
+    const offset = 20;
+    if (backBlock.offsetTop - scrollPosition <= viewportHeight - offset){
+      backBtn.classList.add('visible');
+      console.log('я чат GPT молодец');
+    } 
+    else {
+      backBtn.classList.remove('visible');
+    }
+  }
+  window.addEventListener('scroll', handleScroll);
+  backBtn.scrollIntoView({
+    behavior: 'smooth'
+  });
+
+  backBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    let targetElement = document.getElementById('header');
+    targetElement.scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
+});
+
 
--- js/script.js
+++ js/script.js
@@ -15,39 +15,78 @@ document.addEventListener('DOMContentLoaded', () => {
     let animatedOffers = document.getElementsByClassName('solutions__item');
     let animatedLogo = document.getElementsByClassName('projects__logo');
     let visibleLogos = Array.from(animatedLogo).filter(isElementInViewport);
-
+    let solutionsWrapper = document.querySelector('.solutions__wrapper');
+    let solutionTitle = document.querySelector('.solutions__title');
+    let solutionSubtitle = document.querySelector('.solutions__subtitle');
+    let solutionBtn = document.querySelector('.solutions__btn');
+  
+    solutionsWrapper.style.opacity = '0';
+  
+    setTimeout(() => {
+      solutionsWrapper.style.opacity = '1';
+    }, 300);
+  
+    setTimeout(() => {
+      if (isElementInViewport(solutionTitle)) {
+        solutionTitle.classList.add('show');
+      }
+    }, 500);
+  
+    setTimeout(() => {
+      if (isElementInViewport(solutionSubtitle)) {
+        solutionSubtitle.classList.add('show');
+      }
+    }, 700);
+  
+    setTimeout(() => {
+      if (isElementInViewport(solutionBtn)) {
+        solutionBtn.classList.add('show');
+      }
+    }, 800);
+  
     for (var i = 0; i < animatedElements.length; i++) {
       var element = animatedElements[i];
-
+  
       if (isElementInViewport(element)) {
         element.classList.add('show');
       }
     }
-
+  
     for (var i = 0; i < animatedOffers.length; i++) {
-      var element = animatedOffers[i];
-
-      setTimeout(function (el) {
+      let element = animatedOffers[i];
+      let logoElement = element.querySelector('.solutions__img');
+      let textElement = element.querySelector('.solutions__text');
+      let iconElement = element.querySelector('.solutions__icon');
+      let delay = i * 500;
+  
+      setTimeout(function (el, logoEl, textEl, iconEl) {
         return function () {
           if (isElementInViewport(el)) {
-            el.classList.add('ok');
+            if (!el.classList.contains('ok')) {
+              logoEl.style.opacity = '1';
+              textEl.style.opacity = '1';
+              iconEl.style.opacity = '1';
+              el.classList.add('ok');
+            }
           }
         };
-      }(element), i * 300);
+      }(element, logoElement, textElement, iconElement), delay);
     }
-
+  
     let randomIndexes = Array.from({ length: visibleLogos.length }, (_, index) => index);
     randomIndexes.sort(() => Math.random() - 0.5);
-
+  
     randomIndexes.forEach((index, i) => {
       const element = visibleLogos[index];
-      const delay = i * 250;
+      const delay = i * 300;
       setTimeout(() => {
-        element.classList.add('show');
+        if (!element.classList.contains('show')) {
+          element.classList.add('show');
+        }
       }, delay);
     });
   }
-
+  
   function saveScrollPosition() {
     scrollPosition = window.scrollY || document.documentElement.scrollTop;
     localStorage.setItem('scrollPosition', scrollPosition);
@@ -73,7 +112,50 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  servicesBtn.addEventListener('click', function () {
-    window.location.href = "services.html";
+  // servicesBtn.addEventListener('click', function () {
+  //   window.location.href = "services.html";
+  // });
+
+  // back-arrow-button
+  // Получаем ссылки на блоки
+  const solutionsBlock = document.querySelector('.solutions');
+  const aboutBlock = document.querySelector('.about');
+  const partnersBlock = document.querySelector('.partners');
+  const projectsBlock = document.querySelector('.projects');
+  const footerBlock = document.querySelector('.footer');
+  const backBtn = document.querySelector('.back__btn')
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
+    const viewportHeight = window.innerHeight;
+    const offset = 20;
+    if (
+      solutionsBlock.offsetTop - scrollPosition <= viewportHeight - offset ||
+      aboutBlock.offsetTop - scrollPosition <= viewportHeight - offset ||
+      partnersBlock.offsetTop - scrollPosition <= viewportHeight - offset ||
+      projectsBlock.offsetTop - scrollPosition <= viewportHeight - offset ||
+      footerBlock.offsetTop - scrollPosition <= viewportHeight - offset
+    ) {
+      // backBtn.style.visibility = "visible";
+      backBtn.classList.add('visible');
+      console.log('я чат GPT молодец');
+    } else {
+      // backBtn.style.visibility = "hidden";
+      backBtn.classList.remove('visible');
+    }
+  }
+  window.addEventListener('scroll', handleScroll);
+  backBtn.scrollIntoView({
+    behavior: 'smooth'
+  });
+
+  backBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    let targetElement = document.getElementById('main');
+    targetElement.scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
+
+
